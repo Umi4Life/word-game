a72b03ddfcd91da6b5300391c0bc39a0adcb9806
wit <poowis2011@hotmail.com>
2019-05-30 15:35:13 +0700

    first commit

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public List<string> words = new List<string>(File.ReadAllLines("Assets/Scripts/filtered_words.txt"));
+    public List<string> used_words = new List<string>();
+    [SerializeField] Text scoreText;
+    readonly string DICTIONARY_PATH = "Assets/Scripts/filtered_words.txt";
+
+    public void UpdateScore(string word)
+    {
+        scoreText.text = scoreText.text + word + ", ";
+        used_words.Add(word);
+        words.Remove(word);
+    }
+}
---
+++ Assets/Scripts/WordGame.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WordGame : MonoBehaviour
+{
+    [SerializeField] Text question;
+    [SerializeField] InputField answer;
+    SceneLoader sceneLoader;
+    Score score;
+    [SerializeField] Text timer;
+    float timeLeft = 7.0f;
+
+    void Start()
+    {
+        sceneLoader = FindObjectOfType<SceneLoader>();
+        score = FindObjectOfType<Score>();
+        List<string> words = score.words;
+        NextWord(words[UnityEngine.Random.Range(0, words.Count)]);
+    }
+
+    void NextWord(string lastWord)
+    {
+        timeLeft = 7.0f;
+        answer.text = "";
+        List<String> filtered = score.words.Where(w => w[0] == lastWord[lastWord.Length - 1]).ToList();
+        question.text = filtered[UnityEngine.Random.Range(0,filtered.Count)];
+        score.UpdateScore(question.text);
+
+    }
+
+    public void OnSubmit()
+    {
+        char answer_first = answer.text[0];
+        char answer_last = answer.text[answer.text.Length - 1];
+
+        char question_first = question.text[0];
+        char question_last = question.text[question.text.Length - 1];
+
+        if(
+            answer_first == question_last &&
+            score.words.Contains(answer.text.ToLower()) && 
+            !score.used_words.Contains(answer.text.ToLower())
+            )
+        {
+            score.UpdateScore(answer.text);
+            NextWord(answer.text);
+        }
+
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        timer.text = "Time Left: " + timeLeft.ToString("0.0");
+        if (timeLeft <= 0)
+        {
+            sceneLoader.loadNextScene();
+        }
+    }
+}
